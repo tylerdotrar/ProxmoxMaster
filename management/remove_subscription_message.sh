cd1274d120b0c16d6271a0f8cb044c2229850785
tylerdotrar <tyler@hotbox.zip>
2025-03-16 00:00:01 -0500

    Refined logic & fixed visual output typo.

--- management/remove_subscription_message.sh
+++ management/remove_subscription_message.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # AUTHOR: Tyler McCann (@tylerdotrar)
-# ARBITRARY VERSION NUMBER: 1.0.2
+# ARBITRARY VERSION NUMBER: 1.0.3
 # LINK: https://github.com/tylerdotrar/ProxmoxMaster
 
 
@@ -11,32 +11,42 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 
+
 # Visual formatting of output
 print_yellow() {
   echo -e "$(tput setaf 3)$1$(tput setaf 7)"
 }
+
 
 # Target File(s)
 sub_file='/usr/share/javascript/proxmox-widget-toolkit/proxmoxlib.js'
 sub_file_bak="${sub_file}.bak" # Backup of original file
 
 
-### Step 1: Remove 'No valid subscription' message.
+## Remove 'No valid subscription' message.
 print_yellow "[+] Voiding 'No valid subscription' message from '$sub_file'..."
 
-# Create backup of target file
-cp $sub_file $sub_file_bak
-echo " o  Backup file created: '$sub_file_bak'"
+# Validate patch hasn't already been applied
+if (grep --silent 'Removing subscription message' $sub_file); then
+    echo ' o  Subscription message already patched. Skipping.'
 
-# Get line number of unique string (- 1) and replace contents
-line=$(grep -n 'No valid subscription' $sub_file | cut -f1 -d:)
-line_p2=$((line - 1))
-sed -i "${line_p2}c\\\t\t\t//Removed 'No valid subscription' message\n\t\t\tvoid({ //Ext.Msg.show({" $sub_file
+else
+    # Create backup of target file
+    cp $sub_file $sub_file_bak
+    echo " o  Backup file created: '$sub_file_bak'"
+    
+    # Get line number of unique string (- 1) and replace contents
+    line=$(grep -n 'No valid subscription' $sub_file | cut -f1 -d:)
+    line_p2=$((line - 1))
+    sed -i "${line_p2}c\\\t\t\t//Removing subscription message\n\t\t\tvoid({ //Ext.Msg.show({" $sub_file
+    echo -e " o  Done.\n"
+    
+    
+    ### Step 2: Restart PVE to take effect
+    print_yellow "[+] Restarting 'pveproxy' for changes to take effect..."
+    systemctl restart pveproxy
+    echo " o  You will need to reload your page/clear your cache to see these changes (or open with a private window)."
+fi
+
 echo -e " o  Done.\n"
 
-
-### Step 2: Restart PVE to take effect
-print_yellow "[+] Restarting 'pveproxy' for changes to take effect..."
-systemctl restart pveproxy
-echo " o  You will need to reload your page/clear your cache to see these changes (or open with a private window)."
-echo " o  Done.\n"
